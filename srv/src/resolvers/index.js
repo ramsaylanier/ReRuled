db1eef2b8d251a7874a993f31d6d8c237c11b30b
Ramsay Lanier <ramsaylanier@gmail.com>
2018-02-09 12:39:16 -0500

    update server schema

--- srv/src/resolvers/index.js
+++ srv/src/resolvers/index.js
@@ -1,11 +1,13 @@
 const { Query } = require('./Query')
 const { auth } = require('./Mutation/auth')
 const { game } = require('./Mutation/game')
+const { rule } = require('./Mutation/rule')
 
 module.exports = {
   Query,
   Mutation: {
     ...auth,
     ...game,
+    ...rule
   }
 }
