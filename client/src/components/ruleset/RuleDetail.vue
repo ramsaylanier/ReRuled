fee7aea800f6793a9dc0a693cb83dcb1f384a524
Ramsay Lanier <ramsaylanier@gmail.com>
2018-02-09 15:53:47 -0500

    add rulesets

---
+++ client/src/components/ruleset/RuleDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ruleset.name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rule-detail',
+  props: ['ruleset']
+}
+</script>
+
+<style>
+
+</style>
--- srv/src/resolvers/index.js
+++ srv/src/resolvers/index.js
@@ -2,12 +2,14 @@ const { Query } = require('./Query')
 const { auth } = require('./Mutation/auth')
 const { game } = require('./Mutation/game')
 const { rule } = require('./Mutation/rule')
+const { ruleset } = require('./Mutation/ruleset')
 
 module.exports = {
   Query,
   Mutation: {
     ...auth,
     ...game,
-    ...rule
+    ...rule,
+    ...ruleset
   }
 }
