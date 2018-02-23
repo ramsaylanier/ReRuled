abc833d6f8006296123f8d3e5506ac298d4a35bc
Ramsay Lanier <ramsaylanier@gmail.com>
2018-02-23 08:44:40 -0500

    add touch context menu to rule list item

---
+++ client/src/modules/rule/list-item/rule-list-item.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="rule">
+    <v-touch class="link" ref="link"
+      v-on:panstart="handlePanStart"
+      v-on:panmove="handlePanMove"
+      v-on:panend="handlePanEnd"
+    >
+      <router-link :to="{name: 'Game Rule', params: {id: rule.id}}">
+        <span class="text">{{rule.ruleText}}</span>
+      </router-link>
+    </v-touch>
+    <ul :class="['sub-menu', dragDirection]">
+      <li class="sub-menu-item">
+        <button>
+          <svg class="icon">
+            <use xlink:href="#delete-icon"/>
+          </svg>
+        </button>
+      </li>
+      <li class="sub-menu-item">
+        <button>
+          <svg class="icon">
+            <use xlink:href="#add-to-ruleset-icon"/>
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script src="./rule-list-item.js"></script>
+<style scoped lang="scss" src="./rule-list-item.scss"></style>
---
+++ client/src/modules/rule/list/rule-list.js
@@ -0,0 +1,14 @@
+// Containers
+import RulesContainer from '@/containers/RulesContainer.vue'
+
+// Components
+import List from '@/components/list/List.vue'
+import RuleListItem from '@/modules/rule/list-item/rule-list-item.vue'
+
+export default {
+  name: 'game-rules',
+  props: ['game'],
+  components: {
+    RulesContainer, List, RuleListItem
+  }
+}
